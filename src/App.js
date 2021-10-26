b095d70b38e6f36e0d31a7304e43a538775dd62d
Aleksandar Ristić <aristic665@gmail.com>
2021-10-26 13:36:05 +0200

    add: logged in and protected routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 import { Home, Browse, Signin, Signup } from './pages'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 
 export default function App() {
+	const user = null
+
 	return (
 		<>
 			<Router>
-				<Route exact path={ROUTES.HOME}>
-					<Home />
-				</Route>
-				<Route exact path={ROUTES.BROWSE}>
-					<Browse />
-				</Route>
-				<Route exact path={ROUTES.SIGN_IN}>
-					<Signin />
-				</Route>
-				<Route exact path={ROUTES.SIGN_UP}>
-					<Signup />
-				</Route>
+				<Switch>
+					<ProtectedRoute user={user} path={ROUTES.BROWSE}>
+						<Browse />
+					</ProtectedRoute>
+					<IsUserRedirect user={user} loggedInPath={ROUTES.HOME} exact>
+						<Home />
+					</IsUserRedirect>
+					<IsUserRedirect
+						user={user}
+						loggedInPath={ROUTES.BROWSE}
+						path={ROUTES.SIGN_IN}
+					>
+						<Signin />
+					</IsUserRedirect>
+					<IsUserRedirect
+						user={user}
+						loggedInPath={ROUTES.BROWSE}
+						path={ROUTES.SIGN_UP}
+					>
+						<Signup />
+					</IsUserRedirect>
+				</Switch>
 			</Router>
 		</>
 	)
